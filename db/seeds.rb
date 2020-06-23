8a40a6d64343196a8a1df770cd09fb4fdf559dc8
sfespinosa <scottjames.espinosa@gmail.com>
2020-06-22 23:06:59 -0700

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.destroy_all
+
+Student.create(first_name: "Daenerys", last_name: "Targaryen")
