3cdd7879fe5f7e24274524a9cda1b6900e574a65
iona-b <ionabrabender@gmail.com>
2020-06-22 20:52:28 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+    def activate
+      @student = Student.find(params[:id])
+      if @student.active == true
+        @student.active = false
+      else
+        @student.active = true
+      end
+      @student.save
+      redirect_to student_path(@student)
+    end
+
 end
