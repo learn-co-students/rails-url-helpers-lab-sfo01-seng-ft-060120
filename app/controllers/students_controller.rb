e89de72d29af3f58c91e9d48b4c38f593064b269
keeks5456 <Akikogreen08@gmail.com>
2020-06-23 17:34:46 -0700

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,7 +6,22 @@ class StudentsController < ApplicationController
   end
 
   def show
+    @student = Student.find(params[:id])
+    # byebug
   end
+# its asking us the show a specific student we are asking to find
+
+  def activate 
+    @student = Student.find(params[:id])
+   if @student.active == true 
+    @student.update(active: "false")
+   else
+    @student.active == false 
+    @student.update(active: "true")
+   end
+   redirect_to student_path(@student.id)
+  end
+
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/students', to: 'students#index'
+  get '/students/:id', to: 'students#show', as: 'student'
+  get '/students/:id/activate', to: 'students#activate', as: "activate_student"
 end
+
+# A user should be able to navigate to /students to view all of the students
+# A user should be able to click the student's name on the index page and be
+#  taken to that specific student's show page.
